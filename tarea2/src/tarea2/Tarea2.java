69418aa158c5a4af8087a64ab383eb2f4d56e1a9
Alex Jara <alejara2021@udec.cl>
2022-10-29 19:32:53 -0300

    Update Tarea2.java
    main changes

--- tarea2/src/tarea2/Tarea2.java
+++ tarea2/src/tarea2/Tarea2.java
@@ -3,8 +3,21 @@ package tarea2;
 
 public class Tarea2 {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) {        
+        
+        Moneda m1 = new Moneda500();        
+        Moneda m2 = new Moneda1000();
+        Moneda m3 = new Moneda1500();
+        Moneda n = null;
+        
+        Expendedor expendedor = new Expendedor(5, 1000);
+        
+        Comprador pedro = new Comprador(m1, 1 , expendedor);
+        Comprador bastian = new Comprador(m2, 2 , expendedor);
+        Comprador alex = new Comprador(m3, 3 , expendedor);
+                       
+        
+        
     }
 
 }
